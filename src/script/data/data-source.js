d1373b518ce55c4fd120292a1a76af499b011a0b
rmdhfz <hfzrmd@gmail.com>
2020-07-29 08:55:35 +0700

    Update: refactor searchClub()

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,42 +1,19 @@
 import clubs from './clubs.js';
 
 class DataSource {
-    // constructor(onSuccess, onFailed){
-    //     this.onSuccess = onSuccess;
-    //     this.onFailed  = onFailed;
-    // }
-    static searchClub(keyword){
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-            
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                // this.onFailed(keyword + " is not found");
-                reject(`${keyword} is not found`);
-            } 
-        });
-    }
+   static searchClub(keyword) {
+       return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`)
+       .then(response => {
+           return response.json();
+       })
+       .then(responseJson => {
+           if(responseJson.teams) {
+               return Promise.resolve(responseJson.teams);
+           } else {
+               return Promise.reject(`${keyword} is not found`);
+           }
+       })
+   }
 }
-
-// function DataSource(onSuccess, onFailed) {
-//     this.onSuccess = onSuccess;
-//     this.onFailed = onFailed;
-// }
-
-// DataSource.prototype.searchClub = function (keyword) {
-
-//     // const filteredClubs = clubs.filter(function (club) {
-//     //     return club.name.toUpperCase().includes(keyword.toUpperCase());
-//     // });
-
-//     const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-//     if (filteredClubs.length) {
-//         this.onSuccess(filteredClubs);
-//     } else {
-//         // this.onFailed(keyword + " is not found");
-//         this.onFailed(`${keyword} is not found`);
-//     }
-// };
+ 
 export default DataSource;
